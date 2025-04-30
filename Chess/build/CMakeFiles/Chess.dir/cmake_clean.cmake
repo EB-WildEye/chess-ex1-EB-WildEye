6eafd2aa325653f8693a4580c288a79b4908e33f
EB-303 <wildeye.eb@gmail.com>
2025-04-30 15:49:30 +0300

    Correct version of Chess 1

---
+++ Chess/build/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/src/Bishop.cpp.o"
+  "CMakeFiles/Chess.dir/src/Bishop.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/Board.cpp.o"
+  "CMakeFiles/Chess.dir/src/Board.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/Chess.cpp.o"
+  "CMakeFiles/Chess.dir/src/Chess.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/GameEngine.cpp.o"
+  "CMakeFiles/Chess.dir/src/GameEngine.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/King.cpp.o"
+  "CMakeFiles/Chess.dir/src/King.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/Queen.cpp.o"
+  "CMakeFiles/Chess.dir/src/Queen.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/Rook.cpp.o"
+  "CMakeFiles/Chess.dir/src/Rook.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/main.cpp.o"
+  "CMakeFiles/Chess.dir/src/main.cpp.o.d"
+  "Chess"
+  "Chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
